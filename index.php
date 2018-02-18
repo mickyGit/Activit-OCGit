05e08ad8741e2203aa50b6e3c3fa92d9c9249617
Leriche-Mickael <leriche.mickael.pro@gmail.com>
2018-02-18 12:03:51 +0100

    création du projet

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Activité 2 OC</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Mon premier projet avec GIT</h1>
+    </body>
+</html>
